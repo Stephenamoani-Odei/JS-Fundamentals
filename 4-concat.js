beb74c72da10f09b1d12686d1635f12825fc6f3b
Stephen Odei <213004223+Stephenamoani-Odei@users.noreply.github.com>
2025-07-16 18:40:38 +0100

    Add concat file

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+
+   const nameOfarg1 = "C is cool";
+   
+    nameOfarg2= nameOfarg1 [''];
+    nameOfarg3 = nameOfarg2;
+
+    console.log(nameOfarg1)
+    console.log( 'C is ', nameOfarg2);
+    console.log(nameOfarg3, 'is', nameOfarg2);
