9c51870dd20e8523fd4be829921ec97bc0923be7
Stephen Odei <213004223+Stephenamoani-Odei@users.noreply.github.com>
2025-07-16 16:10:51 +0100

    New ifs added

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const age = 20;
+
+if (arg === 20){
+    console.log('No argument'); 
+} else if(arg <= 20) {
+    console.log('Argument found');
+} else (arg > 20 ); {
+    console.log('Arguments found');
+}
