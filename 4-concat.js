06f168e3df2b393e9b6f02a46889ebdc16ec6cc1
STEPHEN ODEI AMOANI <odeiamoani98@gmail.com>
2025-07-16 19:38:04 +0100

    Update 4-concat.js
    Add HBTN

--- 4-concat.js
+++ 4-concat.js
@@ -5,5 +5,5 @@
     nameOfarg3 = nameOfarg2;
 
     console.log(nameOfarg1)
-    console.log( 'C is ', nameOfarg2);
+    console.log( 'HBTN is ', nameOfarg2);
     console.log(nameOfarg3, 'is', nameOfarg2);
