90d7567995637be65b359d899184a76adc73429c
STEPHEN ODEI AMOANI <odeiamoani98@gmail.com>
2025-07-16 22:45:01 +0100

    Update 2-arguments.js
    few changes

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,7 +2,7 @@ const age = process.argv.slice(2);
 
 if (age.length === 0){
     console.log('No argument'); 
-} else if(age.length === 3) {
+} else if(age.length === 1) {
     console.log('Argument found');
 } else  {
     console.log('Arguments found');
