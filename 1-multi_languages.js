cab7133d08beeb5a1e599328888759dc940d9176
STEPHEN ODEI AMOANI <odeiamoani98@gmail.com>
2025-07-17 01:34:40 +0100

    Update 1-multi_languages.js
    Add multi languages

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,9 +1,7 @@
-let myFun = ' C is fun';
-let myPy = 'Python is amazing';
-let myJay = 'Javascript is amazing';
-console.log(myFun);
-console.log(mypy);
-console.log(myJay);
+
+console.log('C is fun');
+console.log('Python is amazing');
+console.log('JavaScript is amazing');
 
 
 
